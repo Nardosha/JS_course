60126f9fa0e8ebd1e45b336cf83a47d5385b1e5c
Nardosha <lalabumlala@gmail.com>
2021-10-16 14:39:15 +0300

    Finished JS_4

--- JS_4_Hover_board/js/script.js
+++ JS_4_Hover_board/js/script.js
@@ -1,4 +1,15 @@
 const board = document.querySelector('#board')
+const colors = [
+  '#9F3E9A',
+  '#F66A98',
+  '#FF8997',
+  '#64ADF1',
+  '#C15AA8',
+  '#7B68EE',
+  '#87CEFA',
+  '#F08080',
+]
+
 const SQUARES_NAMBER = 500
 
 for (let i = 0; i < SQUARES_NAMBER; i++) {
@@ -12,8 +23,15 @@ for (let i = 0; i < SQUARES_NAMBER; i++) {
 }
 
 function setColor(element) {
-  element.style.background = 'red'
+  const color = getRandomColor()
+  element.style.background = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 function removeColor(element) {
-  element.style.background = 'black'
+  element.style.background = '#1d1d1d'
+  element.style.boxShadow = `0 0 2px #000`
+}
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
 }
